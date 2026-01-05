1e238b935539777e8e87aae703bbbb410562a220
Anubhav-Tomar <122211486+Anubhav-Tomar@users.noreply.github.com>
2026-01-05 19:03:00 +0530

    Update resume link in personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -14,5 +14,5 @@ export const personalData = {
   stackOverflow: "https://stackoverflow.com/users/28064467/anubhav-tomar",
   leetcode: "https://leetcode.com/u/Anubhav_Tomar/",
   devUsername: "anubhavtomar",
-  resume: "https://drive.google.com/file/d/1Aks1WrrQSXNXPv2GIjO0fQ1_htwMasEA/view?usp=share_link",
+  resume: "https://drive.google.com/file/d/1nSMTuBe7m_lhgTf6My-oiWwuE_t2-9Pv/view?usp=share_link",
 };
